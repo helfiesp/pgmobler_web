1c35ec5bab20594a02b9da777c0f0ac010b996e7
helfiesp <59867398+helfiesp@users.noreply.github.com>
2024-02-10 16:51:59 +0100

    Changes to recent products

--- website_app/context_processors.py
+++ website_app/context_processors.py
@@ -6,11 +6,13 @@ def base_info(request):
     text_areas = models.text_areas.objects.first()
     footer_textareas = models.footer_textareas.objects.first()
     business_information = models.business_information.objects.first()
+    recent_products = models.product.objects.filter(enabled=True).order_by('-date_added')[:8]
 
     return {
         'all_products': products,
         'all_categories': categories,
         'text_areas': text_areas,
         'footer_textareas': footer_textareas,
-        'business_information': business_information
+        'business_information': business_information,
+        'recent_products': recent_products,
     }
