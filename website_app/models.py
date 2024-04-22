4a04e74693799a752ce182996623d89baa70d8dd
helfiesp <59867398+helfiesp@users.noreply.github.com>
2024-04-22 11:24:03 +0200

    Update models.py

--- website_app/models.py
+++ website_app/models.py
@@ -26,13 +26,18 @@ class product(models.Model):
     more_information = models.TextField(null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
     date_edited = models.DateTimeField(null=True, blank=True)  
-    
+
     def save(self, *args, **kwargs):
         if not self.string_id:
-            # Combining title and subtitle with a hyphen separator if subtitle is not empty
-            full_title = f"{self.title} - {self.subtitle}" if self.subtitle else self.title
-            self.string_id = slugify(full_title)
-        super().save(*args, **kwargs)
+            base_string_id = slugify(self.title)
+            new_string_id = base_string_id
+            increment = 1
+            # Check if the string_id already exists and increment until a unique one is found
+            while Product.objects.filter(string_id=new_string_id).exists():
+                new_string_id = f"{base_string_id}-{increment}"
+                increment += 1
+            self.string_id = new_string_id
+        super(Product, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.title
