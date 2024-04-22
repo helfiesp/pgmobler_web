53a7983ae6d65fc266e8264f22ae3d122878c0d3
helfiesp <59867398+helfiesp@users.noreply.github.com>
2024-04-22 10:09:50 +0200

    Update models.py

--- website_app/models.py
+++ website_app/models.py
@@ -27,15 +27,25 @@ class product(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
     date_edited = models.DateTimeField(null=True, blank=True)  
 
+    def save(self, *args, **kwargs):
+        if not self.string_id:
+            base_string_id = slugify(self.title)
+            unique_string_id = self.generate_unique_string_id(base_string_id)
+            self.string_id = unique_string_id
+        super().save(*args, **kwargs)
+
+    @staticmethod
+    def generate_unique_string_id(base_id):
+        original_id = base_id
+        count = 1
+        while Product.objects.filter(string_id=base_id).exists():
+            base_id = f"{original_id}-{count}"
+            count += 1
+        return base_id
+
     def __str__(self):
         return self.title
-    
-    def save(self, *args, **kwargs):
-        # This checks if string_id is not already set. If not, it generates it from the title.
-        if not self.string_id:
-            self.string_id = slugify(self.title)
-        super(product, self).save(*args, **kwargs)
-
+        
 class product_image(models.Model):
     product = models.ForeignKey(product, related_name='images', on_delete=models.CASCADE)
     image = models.ImageField(upload_to='product_images/', null=True, blank=True)
