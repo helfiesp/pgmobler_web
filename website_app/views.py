18a31dc08d420ed05002cb2d8cca6b26ceeeedf1
helfiesp <59867398+helfiesp@users.noreply.github.com>
2024-05-10 14:43:41 +0200

    Update views.py

--- website_app/views.py
+++ website_app/views.py
@@ -672,8 +672,9 @@ def add_order(request, customer_id):
 
             # Use cleaned_post instead of request.POST
             for key, value in cleaned_post.items():
-                if "product" in key and "product_info" not in key:
-                    print("YES")
+                if "[product]" in key and "[product_info]" not in key:
+                    print(value)
+                    print(type(value))
                     if isinstance(value, int):
                         value = models.product.objects.filter(pk=value).title
                 match = pattern.match(key)
